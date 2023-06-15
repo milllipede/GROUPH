e22015dca304e08004c83ff87066c06be96093e2
milllipede <134053863+milllipede@users.noreply.github.com>
2023-06-16 00:38:31 +0100

    Add files via upload

---
+++ Group H boostrap project/timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '38-718',
+        subject: 'CSE122',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-718',
+        subject: 'CSE102',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-718',
+        subject: 'CSE152',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '27-304Y',
+        subject: 'CSE152',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '28-107',
+        subject: 'CSE112',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '28-107',
+        subject: 'CSE142',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'CSE142',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'GNS111',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'CACULUS',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'GNS111',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'CSE132',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'CSE112',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'CACULUS',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'CSE112',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'CSE102',
+        type: 'Lecture'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-01 pm',
+        roomNumber: '34-604',
+        subject: 'GNS111',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'CSE122',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'CSE152',
+        type: 'Lecture'
+    }
+]
